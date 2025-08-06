f8e04c41c9a4aa6bfe8d64159c92ec92db3e452e
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-08-06 17:59:43 +0200

    GZABARTE-0 Add cloudfront distribution id to the outputs of terraform module

--- source/deployment/cloudfront/output.tf
+++ source/deployment/cloudfront/output.tf
@@ -2,3 +2,8 @@ output "cloudfront_distribution_arn" {
   description = "ARN of the CloudFront distribution"
   value       = module.cloudfront.cloudfront_distribution_arn
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = module.cloudfront.cloudfront_distribution_id
+}
